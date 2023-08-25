548ff30839bbf3224830bdc8db85838ca609b98d
league <league@imac-39.attlocal.net>
2023-08-24 18:29:47 -0700

    :b:

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,45 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	public static void main(String[] args) {
+		Robot rob1 = new Robot(400,500);
+		Robot rob2 = new Robot (600,600);
+		Robot rob3 = new Robot(800,500);
+		Robot rob4 = new Robot(1000,600);
+		Robot rob5 = new Robot(1200,500);
+		rob1.setPenColor(Color.blue);
+		rob2.setPenColor(Color.yellow);
+		rob3.setPenColor(Color.black);
+		rob4.setPenColor(Color.green);
+		rob5.setPenColor(Color.red);
+		Thread r1 = new Thread(() -> robCircle(rob1));
+		Thread r2 = new Thread(() -> robCircle(rob2));
+		Thread r3 = new Thread(() -> robCircle(rob3));
+		Thread r4 = new Thread(() -> robCircle(rob4));
+		Thread r5 = new Thread(() -> robCircle(rob5));
+		
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+		
+	}
+	private static void robCircle(Robot rob) {
+		rob.turn(-45);
+		rob.setSpeed(15);
+		rob.setPenWidth(5);
+		rob.penDown();
+		for(int i = 0; i <360; i++) {
+			rob.move(3);
+			rob.turn(1);
+		}
+		rob.hide();
+	}
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,37 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		Robot[] robots = new Robot[5];
+		for (int z = 0; z < robots.length; z++) {
+			robots[z] = new Robot(500,600);
+			robots[z].setSpeed(1000);
+		}
+		Thread[] robThread = new Thread[5];
+		Random rand = new Random();
+		
+		for (int i = 0; i < robThread.length; i++) {
+			int k = i;
+			robThread[i] = new Thread(()-> robots[k].move(rand.nextInt(45)+5));
+		}
+		while(isNotZero(robots)) {
+		for(Thread th : robThread) {
+			
+		}
+		}
+	}
+	private static boolean isNotZero(Robot[] robots) {
+		for(Robot r : robots) {
+			if(r.getY()<=0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
